7c92b1ccbbf9ec5bd73559f32cb1e27da190c393
Peter A. <ink.splatters@pm.me>
2024-09-23 09:00:53 +0200

    * migrated to `rye` from `poetry` * using latest deps

---
+++ shell.nix
@@ -0,0 +1,40 @@
+with import <nixpkgs> { };
+let
+  llvmPackages = llvmPackages_18;
+  clang = clang_18;
+  lld = lld_18;
+in
+
+(
+  {
+    clang,
+    lld,
+    llvmPackages,
+    ...
+  }:
+  let
+    inherit (llvmPackages) stdenv libcxx;
+  in
+  mkShell.override { inherit stdenv; } {
+    nativeBuildInputs = [
+      libcxx
+      clang
+      lld
+      gnumake
+      iconv
+    ];
+
+    shellHook = ''
+      export CXXFLAGS="-isystem ${libcxx.dev}/include/c++/v1 -Wall -O3 -mcpu=apple-m1 -funroll-loops -ffast-math -std=c++20 -stdlib=libc++ -flto=thin"
+      export LDFLAGS="-fuse-ld=lld"
+    '';
+#    shellHook = ''
+#      export CXXFLAGS="-isystem ${libcxx.dev}/include/c++/v1 -Wall -O3 -mcpu=apple-m1 -funroll-loops -ffast-math -std=c++20 -stdlib=libc++ -flto=thin"
+#      export LDFLAGS="-fuse-ld=lld"
+#      export CC=clang++
+#    '';
+
+  }
+)
+
+  { inherit clang lld llvmPackages; }
